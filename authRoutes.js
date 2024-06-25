904c79b1a77a4c1a697c83d6cf434d7808f1099b
deoliverrafa <ytsecretgames38@gmail.com>
2024-06-25 12:36:54 -0300

    login update

--- authRoutes.js
+++ authRoutes.js
@@ -30,7 +30,7 @@ router.post('/login', async (req, res) => {
             return res.status(400).json({ message: "Senha incorreta tente novamente", logged: false });
         }
 
-        const token = jwt.sign({user: userFinded}, {expires: '1h'})
+        const token = jwt.sign({user: userFinded})
         
         res.status(202).json({token});
 
