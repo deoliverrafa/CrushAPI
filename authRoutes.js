3f92a3b0cd91451cc620fa3316f53fabda319142
deoliverrafa <ytsecretgames38@gmail.com>
2024-07-02 12:24:28 -0300

    caso null login

--- authRoutes.js
+++ authRoutes.js
@@ -15,6 +15,10 @@ router.post('/login', async (req, res) => {
         const { nickname, password } = req.body;
 
         if (!nickname || !password) {
+            return res.status(400).json({message: "Insira Nome e Senha"})
+        }
+
+        if (!nickname || !password) {
             return res.status(400).json({ message: "Campo faltando", logged: false });
         }
 
