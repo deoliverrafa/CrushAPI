72b7eb6e25558458500e0eae4c3c7aa97c6b1f5d
deoliverrafa <ytsecretgames38@gmail.com>
2024-07-14 17:11:39 -0300

    update userRoutes

--- userRoutes.js
+++ userRoutes.js
@@ -2,16 +2,17 @@ import express from 'express';
 import getConnection from './connection.js';
 import userSchema from './userSchema.js';
 import jwt from 'jsonwebtoken'
-const router = express.Router()
+import mongoose from 'mongoose';
+
+const router = express.Router();
 const dataBase = new getConnection();
 
 router.get("/token/:token", async (req, res) => {
-
     try {
+        const token = req.params.token;
 
-        const token = req.params.token
         if (!token) {
-            return res.status(400).json({ message: "É preciso especificar um token" })
+            return res.status(400).json({ message: "É preciso especificar um token" });
         }
 
         let decodedObj;
@@ -20,56 +21,59 @@ router.get("/token/:token", async (req, res) => {
             decodedObj = jwt.verify(token, process.env.JWT_SECRET);
         } catch (error) {
             if (error.name == "TokenExpiredError") {
-               return res.status(403).json({ message: 'Token Expirado', validToken: false })
+                return res.status(403).json({ message: 'Token Expirado', validToken: false });
             }
-            return res.status(403).json({ message: 'Token Inválido', validToken: false })
+            return res.status(403).json({ message: 'Token Inválido', validToken: false });
         }
 
-        await dataBase.connect()
+        await dataBase.connect();
 
-        if (!token) {
-            return res.status(400).json({ message: "token não especificado", validToken: false })
-        }
-
-        const userFinded = await userSchema.findOne({ _id: decodedObj.user._id })
+        const userFinded = await userSchema.findOne({ _id: decodedObj.user._id });
 
         if (!userFinded) {
-            return res.status(400).json({ message: "Usuário não encontrado" })
+            return res.status(400).json({ message: "Usuário não encontrado" });
         }
 
-        return res.status(200).json({ userFinded })
+        return res.status(200).json({ userFinded });
 
     } catch (error) {
-        return res.status(500).json({ error })
+        return res.status(500).json({ error });
     }
-})
+});
 
 router.get("/id/:id", async (req, res) => {
     try {
-        const id = req.params.id
+        const queryId = req.params.id;
 
-        await dataBase.connect()
+        console.log("ID recebido na rota: ", queryId);
 
-        if (!id) {
-            return res.status(400).json({ message: "id não especificado" })
+        if (!queryId) {
+            return res.status(400).json({ message: "ID não especificado" });
         }
 
-        const userFinded = await userSchema.findOne({ _id: id })
+        // Conecta ao banco de dados
+        await dataBase.connect();
 
-        if (!userFinded || userFinded == null) {
-            return res.status(200).json({
+        // Busca o usuário pelo _id
+        const userFinded = await userSchema.findOne({ _id: queryId });
+        console.log("Usuário encontrado no MongoDB: ", userFinded);
+
+        if (!userFinded) {
+            return res.status(404).json({
+                message: "Usuário não encontrado",
                 nickname: 'Deletado',
                 campus: 'Deletado',
                 email: 'Deletado',
-                avatar: 'path/to/deleted-user-avatar.png' // URL para um avatar padrão
+                avatar: ''
             });
         }
 
-        return res.status(200).json({ userFinded })
+        return res.status(200).json({ userFinded });
 
     } catch (error) {
-        return res.status(500).json({ userFinded: { nickname: 'Deletado', campus: 'Deletado', email: 'Deletado', bio: 'Deletado' } })
+        console.error("Erro ao buscar usuário:", error);
+        return res.status(500).json({ message: "Erro ao buscar usuário", error });
     }
-})
+});
 
 export default router;
